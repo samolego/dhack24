51473a2ba73ab7a9558fe739c96bd67d3801e90f
samolego <34912839+samolego@users.noreply.github.com>
2024-04-20 15:47:17 +0200

    add supabase consts

---
+++ mobile/lib/constants.dart
@@ -0,0 +1,7 @@
+
+
+// The URL of Supabase project
+const DB_URL = 'https://clnlnfebnsppdpspvryr.supabase.co';
+// Public key
+const DB_KEY =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNsbmxuZmVibnNwcGRwc3B2cnlyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTM2MTc5NDQsImV4cCI6MjAyOTE5Mzk0NH0.ml9R-adBNPAeurdrQUJQHlGtNfQ89cSy6_RQwnEWHt8";
