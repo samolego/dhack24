df4cd21b946773b431d8148f1d5a647398242d5c
marindereggi <55254483+marindereggi@users.noreply.github.com>
2024-04-20 17:54:40 +0200

    Added default map view

--- mobile/lib/ui/screen/MapScreen.dart
+++ mobile/lib/ui/screen/MapScreen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
 
 class MapScreen extends StatefulWidget {
   const MapScreen({super.key});
@@ -10,6 +12,25 @@ class MapScreen extends StatefulWidget {
 class _MapScreenState extends State<MapScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return FlutterMap(
+      options: const MapOptions(
+        initialCenter: LatLng(51.509364, -0.128928),
+        initialZoom: 9.2,
+      ),
+      children: [
+        TileLayer(
+          urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+          userAgentPackageName: 'com.example.app',
+        ),
+        RichAttributionWidget(
+          attributions: [
+            TextSourceAttribution(
+              'OpenStreetMap contributors',
+              onTap: () => print('OpenStreetMap contributors'),
+            ),
+          ],
+        ),
+      ],
+    );
   }
 }
