d8cb70559b8177ae10e835fd31f2c8a94b43878e
marindereggi <55254483+marindereggi@users.noreply.github.com>
2024-04-21 10:23:06 +0200

    Added calculation for path

--- mobile/lib/logic/tsp.dart
+++ mobile/lib/logic/tsp.dart
@@ -3,21 +3,26 @@ import 'package:tsp_route_finder/tsp_route_finder.dart';
 
 Future<List<int>> findTSP(final List<FractionalOffset> objectPositions,
     double imgWidth, double imgHeight) async {
-  debugPrint("Not using imgWidth: $imgWidth in imgHeight: $imgHeight!!");
+  debugPrint("I am using imgWidth: $imgWidth in imgHeight: $imgHeight");
 
   if (objectPositions.isEmpty) {
     return [];
   }
 
-  // Define a list locations
-  final List<CitiesLocations> locations = objectPositions
-      .map((e) => CitiesLocations(
-          cityName: e.hashCode.toString(),
-          latitude: e.dy * imgHeight,
-          longitude: e.dx * imgWidth))
-      .toList();
+  var locations = [
+    CitiesLocations(cityName: "Start", latitude: 0, longitude: 0)
+  ];
+
+  locations.addAll(objectPositions.map((e) => CitiesLocations(
+      cityName: e.hashCode.toString(),
+      latitude: e.dy * imgHeight,
+      longitude: e.dx * imgWidth)));
 
   // Calculate the TSP route
   return TspPackage.calculateTspRoute(
-      locations: locations, startingLocationIndex: 0);
+          locations: locations, startingLocationIndex: 0)
+      .then((value) {
+    value.add(0);
+    return value;
+  });
 }
