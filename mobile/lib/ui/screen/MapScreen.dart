086f10c8eeff975d4abce5885c3348263cb723ba
Gasper <suhadolnik.gasper@gmail.com>
2024-04-21 09:56:45 +0200

    fix (kinda) image size

--- mobile/lib/ui/screen/MapScreen.dart
+++ mobile/lib/ui/screen/MapScreen.dart
@@ -15,29 +15,41 @@ class MapScreen extends StatefulWidget {
 class _MapScreenState extends State<MapScreen> {
   @override
   Widget build(BuildContext context) {
+    //final image = Image.asset('assets/map.png', height: 100, width:100);
     final image = Image.asset('assets/map.png');
+
+
     final imageSize = MediaQuery.of(context).size;
     return InteractiveViewer(
       boundaryMargin: const EdgeInsets.all(20.0),
       minScale: 0.1,
       maxScale: 5.6,
       child: Center(
-        child: SizedBox(
-          width: imageSize.width,
-          height: imageSize.height,
+
           child: Stack(
             children: [
               image,
               CustomPaint(
+                size: getImageSize(imageSize),
                 // Use CustomPaint to draw objects on top of the image
                 painter: ProductMarkerPainter(widget.getObjPositions()),
               ),
             ],
           ),
-        ),
       ),
     );
   }
+
+  //method to get image size:
+  //FUJ
+  Size getImageSize(Size imageSize) {
+    var h = 1280;
+    var w = 2412;
+    var h_rel = (imageSize.width / w) * h;
+    var w_rel = imageSize.width;
+    return Size(w_rel,h_rel);
+  }
+
 }
 
 // Custom Painter class to draw objects on canvas
@@ -52,7 +64,6 @@ class ProductMarkerPainter extends CustomPainter {
       ..color = Colors.red
       ..strokeWidth = 2.0
       ..style = PaintingStyle.stroke;
-
     for (var position in objectPositions) {
       final offset =
           Offset(position.dx * size.width, position.dy * size.height);
